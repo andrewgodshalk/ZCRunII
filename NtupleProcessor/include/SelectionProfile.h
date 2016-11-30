2739125592f6d69227c5dfb29dddcea0fadb8e1b
Andrew Godshalk <andrewgodshalk@gmail.com>
2016-11-30 12:03:26 -0600

    Added code to split SP spec string by object.

--- NtupleProcessor/include/SelectionProfile.h
+++ NtupleProcessor/include/SelectionProfile.h
@@ -19,9 +19,9 @@
 #include <string>
 #include <utility>
 #include <vector>
-// Root Classes
 // Project Specific classes
-//#include "EventHandler.h"
+#include "ObjectCriteria.h"
+#include "SelectionProfileCollection.h"
 #include "Logger.h"
 
 class SelectionProfile
@@ -32,25 +32,36 @@ class SelectionProfile
 
   // Accessors.
     const std::string full()     {return fullProfileStr_ ;}
-    const std::string truncated(){return specifierStr_   ;}
+    const std::string specifier_(){return specifierStr_   ;}
     const std::string getObjectCriteriaString(const std::string);
 
   private:
     void breakDownSelectionByObject();
-    // Function that breaks down selection into strings for each object.
-    // Information not completely specified is filled in from default profile.
-    // Final strings are put together into full specifier string.
+      // Function that breaks down selection into strings for each object.
+      // Information not completely specified is filled in from default profile.
+      // Final strings are put together into full specifier string.
+    void getObjectSplitLocations(std::vector<std::size_t>&);
+      // Finds starting location of each object in specifier string. Set to -1 if object is specified.
+    void partionSpecifierString(const std::vector<std::size_t>&, std::vector<std::string>&);
+      // Returns specifier string split by object. Passed references to object start locations and vector of strings to fill.
 
   // String storage.
-    std::string specifierStr_; // String used in constructor. Specifies the differences from default.
-    std::string fullProfileStr_;      // String created by class, filling in holes in specifier string with default values.
+    std::string specifierStr_;    // String used in constructor. Specifies the differences from default.
+    std::string fullProfileStr_;  // String created by class, filling in holes in specifier string with default values.
+
+  // Object Criteria Pointers
+    std::map<std::string, ObjectCriteria*> objCriteria_;
 
   // Static default value for SelectionString
     const static std::string defaultProfileStr_;
+
   // Variables for processing string.
+    static std::vector<std::string> objectSpecifierStrs_;
     static std::map<std::string, std::pair<std::size_t, std::size_t> > objectSubstrRange_;
 
-    Logger logger_    ;
+    Logger logger_;
+
+    friend class SelectionProfileCollection;
 };
 
 #endif
--- NtupleProcessor/src/SelectionProfile.cpp
+++ NtupleProcessor/src/SelectionProfile.cpp
@@ -7,11 +7,11 @@ SelectionProfile.cpp
 */
 
 // Standard Libraries
+#include <algorithm>
 #include <map>
 #include <string>
 #include <utility>
 #include <vector>
-// Root Classes
 // Project Specific classes
 #include "EventHandler.h"
 #include "Logger.h"
@@ -19,13 +19,15 @@ SelectionProfile.cpp
 
 using std::map;
 using std::pair;
-//using std::regex;
 using std::string;
 using std::vector;
 typedef pair<size_t, size_t> str_rng;
 
 // Define default selection profile and object criteria (see doc/SelectionProfile_format.txt)
 const string SelectionProfile::defaultProfileStr_ = "jgZllf0070c0110rcLp20e21id3iso2sfcJ0sip30e25jeccflvaMETptc0000tpfHF0iNoHFSVnsfn";
+
+// Object specifiers strings
+std::vector<std::string> SelectionProfile::objectSpecifierStrs_ = {"j","Z","L","J","MET","HF"};
 
 // Object subtring ranges.
 map<string, str_rng > SelectionProfile::objectSubstrRange_ =
@@ -73,7 +75,60 @@ void SelectionProfile::breakDownSelectionByObject()
 { // Make a full selection string out of the smaller specifier string.
   // String breakdown: j[g] Z[ll][f0070][c0110][nc] L[p20][e21][id3][iso2][sfc] J[0si][p30][e25][jecc][flva] MET[c0000][tpf] HF[0i][NoHF][SVn][sfn]
     //typedef string::const_iterator str_iter;
+    logger_.trace("breakDownSelectionByObject() called");
+
+  // Find object strings within specifier string.
+    vector<size_t> objLocs    (objectSpecifierStrs_.size(), string::npos);
+    vector<string> objSpecStrs(objectSpecifierStrs_.size(), "");
+    getObjectSplitLocations(objLocs);
+    partionSpecifierString (objLocs, objSpecStrs);
 
     fullProfileStr_ = defaultProfileStr_;
     fullProfileStr_[23] = '4';
+}
+
+void SelectionProfile::getObjectSplitLocations(vector<size_t>& locations)
+{ // Find starting locations of each object in specifier string.
+    logger_.trace("getObjectSplitLocations() called");
+
+  // Find each object in the string remaining after the previously found object.
+    size_t searchStartingPoint = 0;  // Set equal to index after last found object string.
+    for(size_t i=0; i<objectSpecifierStrs_.size(); i++)
+    { // Find start location of next object
+        locations[i] = specifierStr_.find(objectSpecifierStrs_[i], searchStartingPoint);
+      // If a valid position was found, set the new search starting point to just after the found object string.
+        if(locations[i]!=string::npos) searchStartingPoint = locations[i]+objectSpecifierStrs_[i].length();
+    }
+
+  // **DEBUG** Print string information.
+    logger_.trace("{}", specifierStr_);
+    logger_.trace("0123456789012345678901234567890123456789012345678901234567890123456789012345678");
+    logger_.trace("0         1         2         3         4         5         6         7        ");
+
+  // Print locations found.
+    for(size_t i=0; i<locations.size(); i++)
+      logger_.trace("Found {} at {}", objectSpecifierStrs_[i], locations[i]);
+}
+
+void SelectionProfile::partionSpecifierString(const std::vector<std::size_t>& locations, std::vector<std::string>& substrings)
+{ // Returns specifier string split by object. Passed references to object start locations and vector of strings to fill.
+    logger_.trace("partionSpecifierString() called");
+  // Find index of first object specified in specifier (use difference in iterators).
+    size_t iThisObj = std::find_if(locations.begin(), locations.end(), [](size_t i){return i!=string::npos;}) - locations.begin();
+    logger_.trace("First object found {}: {} at {}", iThisObj, objectSpecifierStrs_[iThisObj], locations[iThisObj]);
+    size_t iNextObj=iThisObj+1;
+    while(iNextObj<objectSpecifierStrs_.size())
+    { // For each object found...
+        if(locations[iNextObj] != string::npos)
+        {   substrings[iThisObj] = specifierStr_.substr(locations[iThisObj], locations[iNextObj]-locations[iThisObj]);
+            iThisObj=iNextObj;
+        }
+        iNextObj++;
+    }
+  // Set the final string with no endpoint.
+    substrings[iThisObj] = specifierStr_.substr(locations[iThisObj]);
+
+  // Print substrings found.
+    for(size_t i=0; i<substrings.size(); i++)
+      logger_.trace("Spec for {} is {}", objectSpecifierStrs_[i], substrings[i]==""?"not specified":substrings[i]);
 }
